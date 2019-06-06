72bc3aab4f4a5fb98b54f2bc93063699367b3315
Jongwoo Han <han980817@gmail.com>
2019-06-06 22:14:40 +0900

    Modified face sticker func

--- app/src/main/java/com/example/cookandroid/watchme/FaceActivity.java
+++ app/src/main/java/com/example/cookandroid/watchme/FaceActivity.java
@@ -1,13 +1,12 @@
 package com.example.cookandroid.watchme;
 
-import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Display;
@@ -27,88 +26,123 @@ import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
 import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
 import com.google.firebase.ml.vision.face.FirebaseVisionFaceLandmark;
 
+import java.io.InputStream;
 import java.util.List;
 
-public class FaceActivity extends Activity {
 
+public class FaceActivity extends AppCompatActivity {
+    ImageView ImageView_face, imageLC, imageRC;
+    Button button;
     Context mContext;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_face);
-        mContext = this;
 
-        final RelativeLayout RelativeLayout_main = findViewById(R.id.RelativeLayout_face);
+        ImageView_face = (ImageView)findViewById(R.id.ImageView_face);
 
-        FirebaseVisionFaceDetectorOptions options =
-                new FirebaseVisionFaceDetectorOptions.Builder()
-                        .setPerformanceMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
-                        .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
-                        .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
-                        .build();
+        button = (Button)findViewById(R.id.Button_ML);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
 
-        final Bitmap bitmap = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.face);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == 1) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                try {
+                    // 선택한 이미지에서 비트맵 생성
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    final Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+                    // 이미지 표시
+                    ImageView_face.setImageBitmap(img);
+                        mContext = this;
 
-        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+                        final RelativeLayout RelativeLayout_face = findViewById(R.id.RelativeLayout_face);
+                        RelativeLayout_face.removeViewInLayout(imageLC);
+                        RelativeLayout_face.removeViewInLayout(imageRC);
 
-        FirebaseVisionFaceDetector detector = FirebaseVision.getInstance()
-                .getVisionFaceDetector(options);
+                        FirebaseVisionFaceDetectorOptions options =
+                                new FirebaseVisionFaceDetectorOptions.Builder()
+                                        .setPerformanceMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
+                                        .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+                                        .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+                                        .build();
 
-        Task<List<FirebaseVisionFace>> result =
-                detector.detectInImage(image)
-                        .addOnSuccessListener(
-                                new OnSuccessListener<List<FirebaseVisionFace>>() {
-                                    @Override
-                                    public void onSuccess(List<FirebaseVisionFace> faces) {
-                                        // Task completed successfully
-                                        Log.d("FACES", faces.toString());
+                        final FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(img);
 
-                                        Point p = new Point();
-                                        Display display = getWindowManager().getDefaultDisplay();
-                                        display.getSize(p);
+                        FirebaseVisionFaceDetector detector = FirebaseVision.getInstance()
+                                .getVisionFaceDetector(options);
 
+                        Task<List<FirebaseVisionFace>> result =
+                                detector.detectInImage(image)
+                                        .addOnSuccessListener(
+                                                new OnSuccessListener<List<FirebaseVisionFace>>() {
+                                                    @Override
+                                                    public void onSuccess(List<FirebaseVisionFace> faces) {
+                                                        // Task completed successfully
+                                                        Log.d("FACES", faces.toString());
 
+                                                        if(faces.toString().equals("[]")){
+                                                            Toast.makeText(getApplicationContext(),"얼굴을 찾을 수 없습니다.", Toast.LENGTH_SHORT).show();
+                                                        }
 
-                                        for (FirebaseVisionFace face : faces) {
-                                            FirebaseVisionFaceLandmark leftCheek = face.getLandmark(FirebaseVisionFaceLandmark.LEFT_CHEEK);
-                                            float lcx = leftCheek.getPosition().getX();
-                                            float lcy = leftCheek.getPosition().getY();
+                                                        Point p = new Point();
+                                                        Display display = getWindowManager().getDefaultDisplay();
+                                                        display.getSize(p);
 
-                                            FirebaseVisionFaceLandmark rightCheek = face.getLandmark(FirebaseVisionFaceLandmark.RIGHT_CHEEK);
-                                            float rcx =rightCheek.getPosition().getX();
-                                            float rcy =rightCheek.getPosition().getY();
+                                                        for (FirebaseVisionFace face : faces) {
+                                                            FirebaseVisionFaceLandmark leftCheek = face.getLandmark(FirebaseVisionFaceLandmark.LEFT_CHEEK);
+                                                            float lcx = leftCheek.getPosition().getX();
+                                                            float lcy = leftCheek.getPosition().getY();
 
-                                            ImageView imageLC = new ImageView(mContext);
-                                            imageLC.setImageResource(R.drawable.left_whiskers);
-                                            imageLC.setX(p.x * lcx / bitmap.getWidth() - 100);
-                                            imageLC.setY(p.y * lcy / bitmap.getHeight() - 100);
+                                                            FirebaseVisionFaceLandmark rightCheek = face.getLandmark(FirebaseVisionFaceLandmark.RIGHT_CHEEK);
+                                                            float rcx =rightCheek.getPosition().getX();
+                                                            float rcy =rightCheek.getPosition().getY();
 
-                                            imageLC.setLayoutParams(new RelativeLayout.LayoutParams(200,200));
+                                                            imageLC = new ImageView(mContext);
+                                                            imageLC.setImageResource(R.drawable.left_whiskers);
+                                                            imageLC.setX(p.x * lcx / img.getWidth() - 100);
+                                                            imageLC.setY(p.y * lcy / img.getHeight() - 100);
 
-                                            RelativeLayout_main.addView(imageLC);
+                                                            imageLC.setLayoutParams(new RelativeLayout.LayoutParams(150,150));
+                                                            RelativeLayout_face.addView(imageLC);
 
-                                            ImageView imageRC = new ImageView(mContext);
-                                            imageRC.setImageResource(R.drawable.right_whiskers);
-                                            imageRC.setX(p.x * rcx / bitmap.getWidth() - 100);
-                                            imageRC.setY(p.y * rcy / bitmap.getHeight() - 100);
+                                                            imageRC = new ImageView(mContext);
+                                                            imageRC.setImageResource(R.drawable.right_whiskers);
+                                                            imageRC.setX(p.x * rcx / img.getWidth() - 100);
+                                                            imageRC.setY(p.y * rcy / img.getHeight() - 100);
 
-                                            imageRC.setLayoutParams(new RelativeLayout.LayoutParams(200,200));
+                                                            imageRC.setLayoutParams(new RelativeLayout.LayoutParams(150,150));
+                                                            RelativeLayout_face.addView(imageRC);
+                                                        }
+                                                    }
+                                                })
+                                        .addOnFailureListener(
+                                                new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        // Task failed with an exception
+                                                        // ...
 
-                                            RelativeLayout_main.addView(imageRC);
-                                        }
-                                    }
-                                })
-                        .addOnFailureListener(
-                                new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        // Task failed with an exception
-                                        // ...
-                                        Toast.makeText(getApplicationContext(),"얼굴을 찾을 수 없습니다.", Toast.LENGTH_LONG).show();
-                                    }
-                                });
+                                                    }
+                                                });
 
-
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
